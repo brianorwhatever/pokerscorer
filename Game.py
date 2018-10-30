709ed599c754d36286353265ce287607e4777bf8
Brian Richter <brianorwhatever@gmail.com>
2018-10-30 12:09:19 -0700

    tested game

--- Game.py
+++ Game.py
@@ -1,13 +1,17 @@
 import random
 
+from exceptions import ValidationError
 from Card import Card
 from Hand import Hand
 from constants import CARD_VALUES, CARD_SUITS
 
 class Game(object):
-    hands = []
-    
     def __init__(self, player_count):
+        self.hands = []
+        if player_count < 2:
+            raise ValidationError('Not enough players - Minimum is 2')
+        if player_count > 10:
+            raise ValidationError('Too many players - Maximum is 10')
         self.deck = [Card(value, suit) for value in CARD_VALUES for suit in CARD_SUITS]
         random.shuffle(self.deck)
         for player in range(player_count):
--- Hand.py
+++ Hand.py
@@ -2,6 +2,7 @@ from exceptions import ValidationError
 
 class Hand(object):
     def __init__(self, cards):
+        self.cards = []
         if len(cards) != 5:
             raise ValidationError(message='Hand must contain 5 cards')
         self.cards = cards
